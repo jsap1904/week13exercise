4d93cb78777a5c3bf8f7c445725952d9cb765310
jsap1904 <sapinoso.j@gmail.com>
2017-04-11 20:09:53 -0700

    update

--- js/main.js
+++ js/main.js
@@ -11,11 +11,30 @@
 // setTimeout(function(){console.log(today.getFullYear()); }, 2000);
 // }
 
-var el = document.getElementById('body')
-var today = new Date()
-function domListenerEvent() {
+// var el = document.getElementById('body')
+// var today = new Date()
+// function domListenerEvent() {
 
-el.textContent = today.getFullYear()
-}
+// el.textContent = today.getFullYear()
+// }
 
-el.addEventListener('click', domListenerEvent(), false)
+// el.addEventListener('click', domListenerEvent(), false)
+
+
+var string = "hey whats up hello"
+
+//given a number, returns the character at that specific index in a string
+console.log(string.charAt("7"))
+
+//finds the first occurrence of a specified vlaue in a string
+console.log(string.indexOf("w"))
+
+//used to split a string into an array of substrings, and returns the new array
+console.log(string.split("hello"))
+
+//removes whitespace from both sides of a string
+console.log(string.trim(""))
+
+//searchss a string for a specified value, and replaces it with a new value you choose
+console.log(string.replace(" ", "SPACE"))
+
